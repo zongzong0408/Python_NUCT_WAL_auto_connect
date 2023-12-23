8eff400f4441729ef3e8aa02c1898c664d2c7e3f
zongzong0408 <zongzongchu0408@gmail.com>
2023-12-23 20:28:04 +0800

    2023/12/23 20:28

--- auto_git.py
+++ auto_git.py
@@ -21,10 +21,8 @@ def git_auto_commit_push(repo_path, commit_message):
 
         print(f"system DETAIL:\t {e}")
 
-# git_repo_path = input("Enter the path to your Git repository: ")
 git_repo_path = "D:\Code\GitHub\zongzongchu0408@gmail.com\Python_NUCT_WAL_auto_connect"
 
-# commit_message = input("Enter the commit message: ")
 current_time = datetime.now()
 commit_message = formatted_time = current_time.strftime("%Y/%m/%d %H:%M")
 
