df3e365122ee06d80a784784384e928e51977e30
zongzong0408 <zongzongchu0408@gmail.com>
2024-02-26 17:22:33 +0800

    2024/02/26 17:22

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 import re
 import subprocess
 
-output = subprocess.check_output(["ipconfig"], universal_newlines=True)
-gateway_match = re.search(r"預設閘道", output)
+def get_default_gateway():
+    try:
+        ipconfig_result = subprocess.check_output(['ipconfig'], stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+        # Splitting the output by lines
+        ipconfig_lines = ipconfig_result.split('\n')
+        # Finding the line containing the default gateway
+        for line in ipconfig_lines:
+            if 'Default Gateway' in line:
+                # Extracting the default gateway IP address
+                default_gateway = line.split(':')[-1].strip()
+                return default_gateway
+    except subprocess.CalledProcessError as e:
+        print("Error executing ipconfig:", e.output)
 
-print(gateway_match)
+# Get and print the default gateway
+default_gateway = get_default_gateway()
+if default_gateway:
+    print("Default gateway:", default_gateway)
+else:
+    print("Default gateway not found.")
