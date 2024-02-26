7fcb6f1ceb40ad9af2f6025b2a250c723f3259c3
zongzong0408 <zongzongchu0408@gmail.com>
2024-02-26 17:14:46 +0800

    2024/02/26 17:14

--- test.py
+++ test.py
@@ -2,6 +2,5 @@ import re
 import subprocess
 
 output = subprocess.check_output(["ipconfig"], universal_newlines=True)
-print(output)
 gateway_match = re.search(r"預設閘道*: ([\d.]+)", output)
 print(gateway_match)
