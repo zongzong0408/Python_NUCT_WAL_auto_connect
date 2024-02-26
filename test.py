59d45c6028fda9c9b502f53b9d75e0aeb1612ae6
zongzong0408 <zongzongchu0408@gmail.com>
2024-02-26 17:10:32 +0800

    2024/02/26 17:10

--- test.py
+++ test.py
@@ -3,3 +3,4 @@ import subprocess
 
 output = subprocess.check_output(["ipconfig", "/all"], universal_newlines=True)
 gateway_match = re.search(r"Default Gateway.*: ([\d.]+)", output)
+print(gateway_match)
