1a7f706a4b454bd7fd5750748caf5e6e4bc42fdc
zongzong0408 <zongzongchu0408@gmail.com>
2024-02-28 15:15:06 +0800

    2024/02/28 15:15

--- auto-git.py
+++ auto-git.py
@@ -10,9 +10,9 @@ def git_commit_push(repo_path, commit_message) -> None:
     try:
 
         os.chdir(repo_path)
-        os.system('git add .')
-        os.system(f'git commit -m "{commit_message}"')
-        os.system('git push origin main')
+        os.system("git add .")
+        os.system(f"git commit -m '{commit_message}'")
+        os.system("git push origin main")
         
         sys.stdout.write("\nsystem OK:\t git commit and push successful!")
 
@@ -28,10 +28,10 @@ def main() -> None:
 
     while (True):
 
-        git_repo_path = os.getcwd()
+        git_repo_path   = os.getcwd()
 
-        current_time = datetime.now()
-        commit_message = current_time.strftime("%Y/%m/%d %H:%M")
+        current_time    = datetime.now()
+        commit_message  = current_time.strftime("%Y/%m/%d %H:%M")
 
         git_commit_push(git_repo_path, commit_message)
 
