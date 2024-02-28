8e20bd5edf0b8c93d46e4fac265bbee52d8fd34d
zongzong0408 <zongzongchu0408@gmail.com>
2024-02-28 15:23:15 +0800

    2024/02/28 15:23

--- auto-git.py
+++ auto-git.py
@@ -3,23 +3,27 @@ import time
 import sys
 import os
 
+AUTO_GIT_WAIT_TIME = 30
+
 def git_commit_push(repo_path, commit_message) -> None:
 
-    sys.stdout.write(f"\n\nsystem INFO:\t start git process.\n")
+    sys.stdout.write("----------------------------------------------\n")
+    sys.stdout.write(f"system INFO:\t start git process.\n\n")
 
     try:
 
         os.chdir(repo_path)
         os.system("git add .")
-        os.system(f"git commit -m '{commit_message}'")
+        os.system(f'git commit -m "{commit_message}"')
         os.system("git push origin main")
         
-        sys.stdout.write("\nsystem OK:\t git commit and push successful!\n\n")
-        sys.stdout.write("===============================================\n\n")
+        sys.stdout.write("\n\nsystem OK:\t git commit and push successful!\n")
+        sys.stdout.write("----------------------------------------------\n\n\n\n")
 
     except Exception as e:
 
-        sys.stdout.write(f"system DETAIL:\t {e}\n\n")
+        sys.stdout.write(f"system DETAIL:\t {e}\n")
+        sys.stdout.write("----------------------------------------------\n\n\n\n")
 
     else:
 
@@ -27,16 +31,21 @@ def git_commit_push(repo_path, commit_message) -> None:
 
 def main() -> None:
 
+    git_repo_path = os.getcwd()
+
+    times = 1
+
     while (True):
 
-        git_repo_path   = os.getcwd()
+        current_time = datetime.now()
+        commit_message = current_time.strftime("%Y/%m/%d %H:%M")
 
-        current_time    = datetime.now()
-        commit_message  = current_time.strftime("%Y/%m/%d %H:%M")
+        sys.stdout.write(f"system INFO:\t auto git times is: {times}\n")
+        sys.stdout.write(f"system INFO:\t now times is: {commit_message}\n")
 
         git_commit_push(git_repo_path, commit_message)
 
-        time.sleep(5)
+        time.sleep(AUTO_GIT_WAIT_TIME)
 
     # return
 
