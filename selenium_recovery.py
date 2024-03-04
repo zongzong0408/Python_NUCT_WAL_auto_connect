ad680d5be25205ea8ab149abdd4d4d56d2e0e4cd
zongzong0408 <zongzongchu0408@gmail.com>
2024-03-04 17:57:14 +0800

    2024/03/04 17:57

--- selenium_recovery.py
+++ selenium_recovery.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium import webdriver
 import os
 
@@ -6,7 +5,7 @@ path = os.getcwd() + r"\driver\firefoxdriver.exe"
 
 print(path)
 
-browser = webdriver.Firefox(executable_path = FirefoxBinary(path))
+browser = webdriver.Firefox(executable_path = path)
 
 browser.get("www.google.com.tw")
 
