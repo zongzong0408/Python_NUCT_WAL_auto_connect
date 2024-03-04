ba025abb5fa41ac61d9700e91cebcdba9fd15ee7
zongzong0408 <zongzongchu0408@gmail.com>
2024-03-04 17:52:20 +0800

    2024/03/04 17:52

--- selenium_recovery.py
+++ selenium_recovery.py
@@ -5,7 +5,7 @@ path = os.getcwd() + "/driver/firefoxdriver.exe"
 
 print(path)
 
-browser = webdriver.Firefox(executable_path = r"D:\GitHub\zongzongchu0408@gmail.com\Python_NUCT_WAL_auto_connect\driver\firefoxdriver.exe")
+browser = webdriver.Firefox(executable_path = "D:\GitHub\zongzongchu0408@gmail.com\Python_NUCT_WAL_auto_connect\driver\firefoxdriver.exe")
 
 browser.get("www.google.com.tw")
 
