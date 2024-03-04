faf9105788b8677164c7e3a14e21f71c8f249867
zongzong0408 <zongzongchu0408@gmail.com>
2024-03-04 18:12:01 +0800

    2024/03/04 18:12

--- selenium_recovery.py
+++ selenium_recovery.py
@@ -3,13 +3,13 @@ from selenium import webdriver
 import os
 import time
 
-path = os.getcwd() + r"\driver\firefoxdriver.exe"
+path = os.getcwd() + r"\driver\geckodriver.exe"
 print(path)
 
 options = webdriver.FirefoxOptions()
 options.binary_location = path
 
-browser = webdriver.Firefox(service_log_path=path, options=options)
+browser = webdriver.Firefox(executable_path=path, options=options)
 
 browser.get("www.google.com.tw")
 
